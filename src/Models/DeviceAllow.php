eb9f7c841cc9fae7d761d7ed421bef7eeab9f5e1
iVampireSP.com <im@ivampiresp.com>
2023-01-19 01:49:29 +0800

    改进

--- src/Models/DeviceAllow.php
+++ src/Models/DeviceAllow.php
@@ -2,13 +2,10 @@
 
 namespace ivampiresp\Cocoa\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class DeviceAllow extends Model
 {
-    use HasFactory;
-
     public $fillable = [
         'device_id',
         'type',
