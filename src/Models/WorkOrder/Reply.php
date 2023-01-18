13e96bf7ca7016bbbcc164bf93ccb819a014c83a
iVampireSP.com <im@ivampiresp.com>
2023-01-19 01:50:35 +0800

    添加 返回类型

--- src/Models/WorkOrder/Reply.php
+++ src/Models/WorkOrder/Reply.php
@@ -2,8 +2,9 @@
 
 namespace ivampiresp\Cocoa\Models\WorkOrder;
 
-use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use ivampiresp\Cocoa\Models\User;
 
 class Reply extends Model
 {
@@ -35,12 +36,12 @@ class Reply extends Model
         });
     }
 
-    public function workOrder()
+    public function workOrder(): BelongsTo
     {
         return $this->belongsTo(WorkOrder::class);
     }
 
-    public function user()
+    public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
