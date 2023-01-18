88d3d95a91bd9b19a80025df986f80f54f66cb47
iVampireSP.com <im@ivampiresp.com>
2023-01-19 01:49:23 +0800

    改进

--- src/Models/Device.php
+++ src/Models/Device.php
@@ -2,18 +2,13 @@
 
 namespace ivampiresp\Cocoa\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Device extends Model
 {
-    use HasFactory;
-
     public $fillable = [
         'name',
         'password',
         'client_id',
     ];
-    public mixed $id;
-    public mixed $name;
 }
