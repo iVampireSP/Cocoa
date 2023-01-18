22b9f8005572e6daea0bcd7a2f7101905319c0b0
iVampireSP.com <im@ivampiresp.com>
2023-01-19 01:49:11 +0800

    重命名中间件

---
+++ src/Http/Middleware/RemoteRequest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace ivampiresp\Cocoa\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Http;
+use ivampiresp\Cocoa\Models\User;
+
+class RemoteRequest
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param Request                                       $request
+     * @param Closure(Request): (Response|RedirectResponse) $next
+     *
+     * @return Response|JsonResponse|RedirectResponse
+     */
+    public function handle(Request $request, Closure $next): Response|JsonResponse|RedirectResponse
+    {
+
+        // add json header
+        $request->headers->set('Accept', 'application/json');
+        if (!$request->hasHeader('X-Module-Api-Token')) {
+            return $this->unauthorized();
+        }
+
+        $token = $request->header('X-Module-Api-Token');
+        if ($token !== config('remote.api_token')) {
+            return $this->unauthorized();
+        }
+
+        // if header has X-User-Id
+        if ($request->header('X-User-Id')) {
+            $user = (new User)->where('id', $request->header('X-User-Id'))->first();
+            // if user null
+            if (!$user) {
+                $http = Http::remote()->asForm();
+                $user = $http->get('/users/' . $request->header('X-User-Id'))->json();
+
+                $user = (new User)->create([
+                    'id' => $user['id'],
+                    'name' => $user['name'],
+                    'email' => $user['email'],
+                ]);
+            }
+
+            Auth::guard('user')->login($user);
+        }
+
+        return $next($request);
+    }
+
+    public function unauthorized(): JsonResponse
+    {
+        return response()->json([
+            'message' => 'Unauthorized.'
+        ], 401);
+    }
+}
