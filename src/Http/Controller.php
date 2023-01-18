ae79e7ed548a9bc74e09a17b0df4c0876f00b278
iVampireSP.com <im@ivampiresp.com>
2023-01-19 01:47:49 +0800

    引入 http helper

--- src/Http/Controller.php
+++ src/Http/Controller.php
@@ -5,18 +5,12 @@ namespace ivampiresp\Cocoa\Http;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\Client\PendingRequest;
 use Illuminate\Routing\Controller as BaseController;
-use Illuminate\Support\Facades\Http;
 use ivampiresp\Cocoa\Helpers\ApiResponse;
+use ivampiresp\Cocoa\Helpers\Http;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests, ApiResponse;
-
-    protected $http;
-
-    public function __construct()
-    {
-        $this->http = Http::remote()->asForm();
-    }
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests, ApiResponse, Http;
 }
