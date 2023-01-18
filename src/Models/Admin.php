1891acede92a37da3caa18122290a9ad336de7c5
iVampireSP.com <im@ivampiresp.com>
2023-01-19 01:49:19 +0800

    移除 sanctum

--- src/Models/Admin.php
+++ src/Models/Admin.php
@@ -4,11 +4,10 @@ namespace ivampiresp\Cocoa\Models;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 class Admin extends Authenticatable
 {
-    use HasApiTokens, Notifiable;
+    use Notifiable;
 
     /**
      * The attributes that are mass assignable.
