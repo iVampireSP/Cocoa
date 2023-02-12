8d4e21eea2ffc41be896dc5e8ce54d460528b525
iVampireSP.com <im@ivampiresp.com>
2023-02-13 01:23:11 +0800

    改进 价格计算

--- src/Models/Host.php
+++ src/Models/Host.php
@@ -107,29 +107,9 @@ class Host extends Model
         });
     }
 
-
-    public function calcPrice(): string
+    protected function calcPrice(): string
     {
-        // 价格计算机会在主机创建和更新时被调用。
-        // 你可以自定义价格计算器，但是请切记，使用 bcmath 函数来计算价格，价格必须是字符串类型。
-        $price = "0";
-
-        /* 以下都是例子，请根据自己的需要来。 */
-        // 加法
-        $price = bcadd($price, "1", 2);
-        // 除法
-        $price = bcdiv($price, "2", 2);
-        // 乘法
-        $price = bcmul($price, "2", 2);
-
-        // 判断是否为 0
-        if (bccomp($price, "0", 2) === 0) {
-            // 如果为 0，就返回 0.01
-            return "0.01";
-        }
-        /* 以上都是例子，请根据自己的需要来。 */
-
-        return $price;
+        return (new HostAction())->calculatePrice($this->toArray());
     }
 
     public function getRouteKeyName(): string
